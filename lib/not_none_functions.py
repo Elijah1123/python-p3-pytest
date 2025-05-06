4eb4323bae2be912905da13e84a69f26130015a6
Elijah M <elijah.macharia@student.moringaschool.com>
2025-05-07 02:24:43 +0300

    Pytest

--- lib/not_none_functions.py
+++ lib/not_none_functions.py
@@ -1,2 +1,7 @@
-def return_not_none():
+# lib/not_none_functions.py
+from not_none_functions_helpers import your_function
+
+def return_not_none(value):
+    if value is not None:
+        return your_function(value)
     return None
---
+++ lib/not_none_functions_helpers.py
@@ -0,0 +1,4 @@
+# lib/not_none_functions_helpers.py
+
+def your_function(value):
+    return f"Processed: {value}"
